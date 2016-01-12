1b29fd12dbed688e6b355f1b7ecd9ab094b8dd83
mystique <13oct08@quantumfoam.uni.me>
2016-01-12 17:31:13 +0530

    Fixed issues, - removed uncessary rows, with customizable function - Removed 'new line' characters in strings
    ToDo :
    - Need to get the links
    - Need to update in Google Spreadsheet

--- moovy/moovy/spiders/spidy.py
+++ moovy/moovy/spiders/spidy.py
@@ -1,27 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import scrapy
-
+import itertools
+import collections
 from moovy.items import MoovyItem
 
-class MoovySpider(scrapy.Spider):
-        name = "moovy"
-        allowed_domains = ["en.wikipedia.org"]
-        start_urls = ["https://en.wikipedia.org/wiki/List_of_Tamil_films_of_2015"]
+# General generator function that skips any number of items from the beginning and end of an iterable
+# http://stackoverflow.com/questions/10079216/skip-first-entry-in-for-loop-in-python
+def skip(iterable, at_start=0, at_end=0):
+	it = iter(iterable)
+	for x in itertools.islice(it, at_start):
+		pass
+	queue = collections.deque(itertools.islice(it, at_end))
+	for x in it:
+		queue.append(x)
+		yield queue.popleft()
 
-        def parse(self,response):
-			#titles = response.selector.xpath('//table[@class="wikitable sortable"]/tr/td/i/a[contains(@href, "title")]')
-			rows = response.selector.xpath('//table[@class="wikitable sortable"]/tr')
-			for index, r1 in enumerate(rows):
-				cells = r1.xpath('.//td')
-				if (len(cells) == 8):
-					#print ("This row has {} cells and month is {}".format(len(cells),str(cells[0].xpath('.//text()').extract())))
-					str1 = cells[0].xpath('.//text()').extract()
-					str1 = map(lambda s: s.strip(), str1)
-					str1 = ''.join(map(str,str1))
-					print ("This row has {} cells and month is {}".format(len(cells), str1))
-					#print("Total score for {} is {}".format(name, score))
-				else:
-					print ('This row has %s cells' % len(cells))
-				for cc,c1 in enumerate(cells):
-					data = c1.xpath('.//text()').extract()
-					print ('This data in this cell is %s' % ''.join(map(str, data)))
-				#print ('End of row')
+
+class MoovySpider(scrapy.Spider):
+    name = "moovy"
+    allowed_domains = ["en.wikipedia.org"]
+    start_urls = ["https://en.wikipedia.org/wiki/List_of_Tamil_films_of_2015"]
+				
+    def parse(self,response):
+		rows = response.selector.xpath('//table[@class="wikitable sortable"]/tr')
+		for index, r1 in enumerate(rows):
+			cells = r1.xpath('.//td')
+			if (len(cells) == 8):
+				#print ("This row has {} cells and month is {}".format(len(cells),str(cells[0].xpath('.//text()').extract())))
+				mMonth = cells[0].xpath('.//text()').extract()
+				# Remove any new line characters from the list  & use map to join the list into single string.
+				mMonth = map(lambda s: s.strip(), mMonth)
+				mMonth = ''.join(map(str,mMonth))
+				
+				mWeek = cells[1].xpath('.//text()').extract()
+				# Remove any new line characters from the list  & use map to join the list into single string.
+				mWeek = map(lambda s: s.strip(), mWeek)
+				mWeek = ''.join(map(str,mWeek))				
+				print ("This row has {} cells and released on the month of {} , week ending {}".format(len(cells), mMonth, mWeek))
+				#list(skip(range(10), at_start=2, at_end=2))
+				cells = list(skip(cells,at_start=2,at_end=1))
+			elif (len(cells) == 7):
+				mWeek = cells[0].xpath('.//text()').extract()
+				# Remove any new line characters from the list  & use map to join the list into single string.
+				mWeek = map(lambda s: s.strip(), mWeek)
+				mWeek = ''.join(map(str,mWeek))
+				print ("This row has {} cells and released on week ending {}".format(len(cells), mWeek))
+				cells = list(skip(cells,at_start=1,at_end=1))
+			else:
+				print ('This row has %s cells' % len(cells))
+				cells = list(skip(cells,at_start=0,at_end=1))
+			for cc,c1 in enumerate(cells):
+				data = c1.xpath('.//text()').extract()
+				#data = data.replace("\n"," ")
+				data = [s.replace('\n',', ') for s in data]
+				#data = map(lambda s: s.strip(), data)
+				print ('The data in this cell is {}'.format(''.join(map(str, data))))
+			#print ('End of row')
+
