30f923e3777b548b7e89ffb86d3ae02e62ce55ff
mystique <13oct08@quantumfoam.uni.me>
2016-01-12 23:44:29 +0530

    Added more comments, removed unwanted code.
    Able to print as necessary for 2015 media

--- moovy/moovy/spiders/spidy.py
+++ moovy/moovy/spiders/spidy.py
@@ -27,6 +27,7 @@ class MoovySpider(scrapy.Spider):
 		for index, r1 in enumerate(rows):
 			mData = []
 			cells = r1.xpath('.//td')
+			# Lets remove the last wiki column as that only has reference			
 			if (len(cells) == 8):
 				#print ("This row has {} cells and month is {}".format(len(cells),str(cells[0].xpath('.//text()').extract())))
 				mMonth = cells[0].xpath('.//text()').extract()
@@ -38,25 +39,31 @@ class MoovySpider(scrapy.Spider):
 				# Remove any new line characters from the list  & use map to join the list into single string.
 				mWeek = map(lambda s: s.strip(), mWeek)
 				mWeek = ''.join(map(str,mWeek))
-				print ("This row has {} cells and released on the month of {} , week ending {}".format(len(cells), mMonth, mWeek))
-				#list(skip(range(10), at_start=2, at_end=2))
+				# print ("This row has {} cells and released on the month of {} , week ending {}".format(len(cells), mMonth, mWeek))
+				# Lets skip the first two cells (Month & Week) and the last one (Wiki Ref Link)
 				cells = list(skip(cells,at_start=2,at_end=1))
 			elif (len(cells) == 7):
 				mWeek = cells[0].xpath('.//text()').extract()
 				# Remove any new line characters from the list  & use map to join the list into single string.
 				mWeek = map(lambda s: s.strip(), mWeek)
 				mWeek = ''.join(map(str,mWeek))
-				print ("This row has {} cells and released on week ending {}".format(len(cells), mWeek))
+				# print ("This row has {} cells and released on week ending {}".format(len(cells), mWeek))
+				# Lets skip the first cell (Week) and the last one (Wiki Ref Link)
 				cells = list(skip(cells,at_start=1,at_end=1))
 			else:
-				print ('This row has %s cells' % len(cells))
+				# print ('This row has %s cells' % len(cells))
+				# Lets skip the last one (Wiki Ref Link)
 				cells = list(skip(cells,at_start=0,at_end=1))
+			
+			# Enumerate over the media Title, Director, Cast, Genre & Production House
 			for cc,c1 in enumerate(cells):
 				data = c1.xpath('.//text()').extract()
-				#data = data.replace("\n"," ")
 				data = [s.replace('\n',', ') for s in data]
 				mData.append(''.join(map(str, data)))
-				#data = map(lambda s: s.strip(), data)
-				print ('Month:{} , Week:{} , in this cell is {}'.format(mMonth,mWeek,''.join(map(str, data))))
-			#print ('Month:{} , Week:{} , in this cell is {}'.format(mMonth,mWeek,'|'.join(map(str, mData))))
-
+				#print ('Month:{} , Week:{} , in this cell is {}'.format(mMonth,mWeek,''.join(map(str, data))))
+			
+			# Check if the movie data is not empty and print out the list with "|" separated line
+			if mData:
+				print ('{}|{}|{}'.format(mMonth,mWeek,'|'.join(map(str, mData))))
+			else:
+				print ('Unable to extract data')
